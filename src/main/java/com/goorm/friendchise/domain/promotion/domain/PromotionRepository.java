ef5d9d3d7b4c3e0b7efd2e5a50b20bcefb0d9030
fbehddn <tom0611@naver.com>
2025-02-06 15:27:43 +0900

    feat: 프로모션 관련 로직 구현

---
+++ src/main/java/com/goorm/friendchise/domain/promotion/domain/PromotionRepository.java
@@ -0,0 +1,6 @@
+package com.goorm.friendchise.domain.promotion.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PromotionRepository extends JpaRepository<Promotion, Long> {
+}
