272f36a6c40c728446c47926556cda0afd7844bd
ksah3756 <ksah3756@gmail.com>
2025-02-06 16:39:48 +0900

    fix: FriendchiseApplication import문 제거

--- src/main/java/com/goorm/friendchise/FriendchiseApplication.java
+++ src/main/java/com/goorm/friendchise/FriendchiseApplication.java
@@ -2,7 +2,6 @@ package com.goorm.friendchise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class FriendchiseApplication {
