f3f9658df639d398a2e253f9d84358a616c51e14
ksah3756 <ksah3756@gmail.com>
2025-02-06 09:26:49 +0900

    feat: Headquarter 서비스 계층 단위 테스트 추가

---
+++ src/test/java/com/goorm/friendchise/domain/headquarter/appilcation/HeadquarterServiceTest.java
@@ -0,0 +1,24 @@
+package com.goorm.friendchise.domain.headquarter.appilcation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeadquarterServiceTest {
+
+    @Test
+    void createHeadquarter() {
+    }
+
+    @Test
+    void getHeadquarter() {
+    }
+
+    @Test
+    void updateHeadquarter() {
+    }
+
+    @Test
+    void deleteHeadquarter() {
+    }
+}
