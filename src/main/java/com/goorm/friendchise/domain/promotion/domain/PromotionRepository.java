3e4fba1e4bd0d36cf528504a45c7250dc8bdff0b
fbehddn <tom0611@naver.com>
2025-02-06 16:24:46 +0900

    feat: FakeRepo 구현을 위한 프로모션 레포 분리

--- src/main/java/com/goorm/friendchise/domain/promotion/domain/PromotionRepository.java
+++ src/main/java/com/goorm/friendchise/domain/promotion/domain/PromotionRepository.java
@@ -1,6 +1,14 @@
 package com.goorm.friendchise.domain.promotion.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
-public interface PromotionRepository extends JpaRepository<Promotion, Long> {
+public interface PromotionRepository {
+	void save(Promotion promotion);
+
+	List<Promotion> findAll();
+
+	Optional<Promotion> findById(Long id);
+
+	void delete(Long id);
 }
