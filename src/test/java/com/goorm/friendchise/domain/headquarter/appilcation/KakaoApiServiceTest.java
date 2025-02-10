0bde4a78263fb4eeaa8c5b1ead344012efbddaaa
ksah3756 <ksah3756@gmail.com>
2025-02-10 15:46:18 +0900

    fix: gpt initialSettingMessage 수정

--- src/test/java/com/goorm/friendchise/domain/headquarter/appilcation/KakaoApiServiceTest.java
+++ src/test/java/com/goorm/friendchise/domain/headquarter/appilcation/KakaoApiServiceTest.java
@@ -1,17 +1,17 @@
-package com.goorm.friendchise.domain.headquarter.appilcation;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class KakaoApiServiceTest {
-
-    @Autowired
-    private KakaoApiService storeRecommendationService;
-
-    @Test
-    void getTotalPlaceData() {
-        System.out.println(storeRecommendationService.getTotalPlaceData());
-    }
-}
+//package com.goorm.friendchise.domain.headquarter.appilcation;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class KakaoApiServiceTest {
+//
+//    @Autowired
+//    private KakaoApiService storeRecommendationService;
+//
+//    @Test
+//    void getTotalPlaceData() {
+//        System.out.println(storeRecommendationService.getTotalPlaceData());
+//    }
+//}
