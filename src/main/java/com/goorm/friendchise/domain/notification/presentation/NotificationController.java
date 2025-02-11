1c57c8fb9bb7933e5321e84a8e346d4742750b7b
fbehddn <tom0611@naver.com>
2025-02-11 12:22:37 +0900

    refactor: 컨트롤러 주입 필드 변경

--- src/main/java/com/goorm/friendchise/domain/notification/presentation/NotificationController.java
+++ src/main/java/com/goorm/friendchise/domain/notification/presentation/NotificationController.java
@@ -1,6 +1,7 @@
 package com.goorm.friendchise.domain.notification.presentation;
 
-import com.goorm.friendchise.domain.notification.application.NotificationService;
+import com.goorm.friendchise.domain.notification.application.NotificationManager;
+import com.goorm.friendchise.domain.notification.application.NotificationSseService;
 import com.goorm.friendchise.domain.notification.dto.response.NotificationResponse;
 import com.goorm.friendchise.domain.notification.dto.response.ReceivedNotificationResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,32 +15,33 @@ import java.util.List;
 @RequestMapping("/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
-	private final NotificationService notificationService;
+	private final NotificationManager notificationManager;
+	private final NotificationSseService notificationSseService;
 
 	// 특정 targetId에 대한 알림 목록 조회 API
 	@GetMapping("/{targetId}")
 	public ResponseEntity<List<ReceivedNotificationResponse>> getNotifications(@PathVariable("targetId") Long targetId) {
-		List<ReceivedNotificationResponse> notifications = notificationService.getNotificationsByTarget(targetId);
+		List<ReceivedNotificationResponse> notifications = notificationManager.getNotificationsByTarget(targetId);
 		return ResponseEntity.ok(notifications);
 	}
 
 	//  알림 읽음 처리
 	@PatchMapping("/{notificationId}/read")
 	public ResponseEntity<NotificationResponse> markAsRead(@PathVariable Long notificationId) {
-		notificationService.markAsRead(notificationId);
+		notificationManager.markAsRead(notificationId);
 		return ResponseEntity.ok(new NotificationResponse(notificationId, "success", "알림이 읽음 처리되었습니다."));
 	}
 
 	//  알림 삭제
 	@DeleteMapping("/{notificationId}")
 	public ResponseEntity<NotificationResponse> deleteNotification(@PathVariable Long notificationId) {
-		notificationService.deleteNotification(notificationId);
+		notificationManager.deleteNotification(notificationId);
 		return ResponseEntity.ok(new NotificationResponse(notificationId, "success", "알림이 삭제되었습니다."));
 	}
 
 	// SSE 구독 (매장 실시간 알림 받기)
 	@GetMapping("/subscribe/{targetId}")
 	public SseEmitter subscribe(@PathVariable("targetId") Long targetId) {
-		return notificationService.subscribe(targetId);
+		return notificationSseService.subscribe(targetId);
 	}
 }
