135ccf422be241dc50b090dc137a0e602fe2af9c
kimjunghwannn <aa2002dcks@naver.com>
2025-02-13 11:25:44 +0900

    feat: add RecommendApi

---
+++ src/main/java/com/goorm/friendchise/domain/customer/domain/Coordinates.java
@@ -0,0 +1,11 @@
+package com.goorm.friendchise.domain.customer.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Coordinates {
+    private final double pointY;
+    private final double pointX;
+}
