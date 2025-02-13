fefacb18edfdff1c92141539b715a3505390e252
kimjunghwannn <aa2002dcks@naver.com>
2025-02-13 11:33:09 +0900

    feat: add RecommendApi

--- src/main/java/com/goorm/friendchise/domain/customer/application/CustomerService.java
+++ src/main/java/com/goorm/friendchise/domain/customer/application/CustomerService.java
@@ -4,15 +4,32 @@ import com.goorm.friendchise.domain.customer.domain.Customer;
 import com.goorm.friendchise.domain.customer.domain.CustomerRepository;
 import com.goorm.friendchise.domain.customer.dto.request.CustomerCreateRequest;
 import com.goorm.friendchise.domain.customer.dto.request.CustomerLoginRequest;
+import com.goorm.friendchise.domain.customer.dto.request.CustomerRecommendStoreRequest;
 import com.goorm.friendchise.domain.customer.dto.response.CustomerDetailResponse;
 import com.goorm.friendchise.domain.customer.dto.response.CustomerPersistResponse;
 import com.goorm.friendchise.domain.customer.dto.response.CustomerTokenResponse;
 import com.goorm.friendchise.domain.customer.exception.CustomerException;
+import com.goorm.friendchise.domain.store.application.StoreService;
+import com.goorm.friendchise.domain.store.domain.Store;
+import com.goorm.friendchise.domain.store.infrastructure.StoreRepository;
+import com.goorm.friendchise.global.auth.util.DistanceCalculator;
 import com.goorm.friendchise.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +37,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class CustomerService {
     private final CustomerRepository customerRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final StoreRepository storeRepository;
+    private final KaKaoApiService kakaoApiService;
+    private final RedisTemplate<String, String> redisTemplate;
+    private static final Logger log = LoggerFactory.getLogger(StoreService.class);
+    private final ConcurrentHashMap<String, Object> locks = new ConcurrentHashMap<>();
+
+    private static final int API_TIMEOUT = 3; // OpenAPI 호출 제한 시간 (초)
+    private static final int MAX_RETRY = 5; // 최대 재시도 횟수
+    private static final long CACHE_EXPIRATION = 10; // 캐싱 지속 시간 (분)
+    private static final String CACHE_PREFIX = "nearestStore:";
+    // 🕒 서비스 실행 시작 시간
+    private Instant serviceStartTime = Instant.now();
 
     @Transactional
     public CustomerPersistResponse create(CustomerCreateRequest request)
@@ -56,5 +85,85 @@ public class CustomerService {
     {
         return customerRepository.findByUsername(username)
                 .orElseThrow(() -> new CustomerException(ErrorCode.USER_NOT_FOUND));
+    }
+    public String findNearestStoreWithCache(CustomerRecommendStoreRequest customerRecommendStoreRequest){
+        String address = customerRecommendStoreRequest.address();
+
+        String cacheKey = CACHE_PREFIX + address;
+        String cachedAddress = redisTemplate.opsForValue().get(cacheKey);
+        if(cachedAddress != null){
+            double elapsedTime = Duration.between(serviceStartTime, Instant.now()).toMillis(); // 실행 시간(ms)
+            log.info("✅ 1차 캐싱 확인됨! 캐시된 매장: " + cachedAddress +"응답시간: " +elapsedTime/1000+"초");
+
+            return cachedAddress;
+        }
+        int attempt;
+        for (attempt = 0; attempt < MAX_RETRY; attempt++) {
+            cachedAddress = redisTemplate.opsForValue().get(cacheKey);
+            if (cachedAddress != null) {
+                double elapsedTime = Duration.between(serviceStartTime, Instant.now()).toMillis(); // 실행 시간(ms)
+                log.info("✅ 2차 캐싱 확인됨! 캐시된 매장: " + cachedAddress +"응답시간: " +elapsedTime/1000+"초");
+                return cachedAddress;
+            }
+
+            String nearestAddress = findNearestStore(address,customerRecommendStoreRequest.franchiseName());
+            if (nearestAddress != null) {
+                if(nearestAddress.contains("cached"))
+                    return nearestAddress.split(" ")[0];
+                double elapsedTime = Duration.between(serviceStartTime, Instant.now()).toMillis(); // 실행 시간(ms)
+                log.info("✅ OPENAPI로 찾은 매장: " + nearestAddress +"응답시간: " +elapsedTime/1000+"초");
+                return nearestAddress;
+            }
+
+            try {
+                TimeUnit.MILLISECONDS.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException("스레드 대기 중 인터럽트 발생", e);
+            }
+        }
+        if(attempt==MAX_RETRY)
+            throw new CustomerException(ErrorCode.RECOMMEND_API_TIMEOUT);
+        else
+            throw new CustomerException(ErrorCode.UNKNOWN_ERROR);
+    }
+    private String findNearestStore(String address,String franchiseName)
+    {
+        String cacheKey = CACHE_PREFIX + address;
+        Object lock = locks.computeIfAbsent(address, key -> new Object());
+        synchronized (lock){
+            try{
+                String cachedAddress = redisTemplate.opsForValue().get(cacheKey);
+                if (cachedAddress != null) {
+                    double elapsedTime = Duration.between(serviceStartTime, Instant.now()).toMillis(); // 실행 시간(ms)
+                    log.info("✅ 3차 캐싱 확인됨! 캐시된 매장: " + cachedAddress +" 응답시간: " +elapsedTime/1000+"초");
+                    return cachedAddress+" cached";
+                }
+                Store nearestStore=calculateNearestStore(address,franchiseName);
+                redisTemplate.opsForValue().set(cacheKey, nearestStore.getAddress(), CACHE_EXPIRATION, TimeUnit.MINUTES);
+                return nearestStore.getAddress();
+            } finally {
+                locks.remove(address);
+            }
+
+        }
+    }
+
+    private Store calculateNearestStore(String address, String franchiseName) {
+        return CompletableFuture
+                .supplyAsync(() -> kakaoApiService.getCoordinatesByAddress(address))
+                .orTimeout(API_TIMEOUT, TimeUnit.SECONDS)
+                .thenApply(customerCoordinates -> {
+                    List<Store> stores = storeRepository.findAll();
+
+                    return stores.stream()
+                            .filter(store -> store.getFranchiseName().equals(franchiseName))
+                            .min(Comparator.comparing(store -> DistanceCalculator.calculateDistance(
+                                    customerCoordinates.getPointY(), customerCoordinates.getPointX(),
+                                    store.getPointY(), store.getPointX()
+                            )))
+                            .orElseThrow(() -> new CustomerException(ErrorCode.NEAR_STORE_NOT_FOUND));
+                })
+                .join();
     }
 }
