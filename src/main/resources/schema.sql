4d55ac82a5c8d301197c19bf4c9a6d9ccc306af9
ksah3756 <ksah3756@gmail.com>
2025-02-18 11:41:25 +0900

    feat: rds에 상권 데이터 한번 넣도록 data.sql 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+create table if not exists friendchise.commercial_area
+(
+    id         bigint auto_increment
+    primary key,
+    area_name  varchar(50)    not null,
+    rental_fee decimal(38, 2) not null,
+    geom       polygon        not null
+    );
+
+create spatial index geom
+    on friendchise.commercial_area (geom);
