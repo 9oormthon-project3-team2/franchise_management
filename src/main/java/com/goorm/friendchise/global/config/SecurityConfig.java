a065cbb8fb7eacc8f9bcb7cde249442ded55f913
kimjunghwannn <157611169+kimjunghwannn@users.noreply.github.com>
2025-02-19 11:37:10 +0900

    Update SecurityConfig.java

--- src/main/java/com/goorm/friendchise/global/config/SecurityConfig.java
+++ src/main/java/com/goorm/friendchise/global/config/SecurityConfig.java
@@ -70,6 +70,7 @@ public class SecurityConfig {
 
 	// TODO 추후 권한별 분리 할때 세분화
 	private static final String[] PUBLIC_ENDPOINTS = {
+		"/customer/register",
 		"/manager/**",
 		"/notifications/**",
 		"/promotions/**"
